1330bf53590820444f6d3d43a4ab1ad3e3aeeda5
Tony Mai <tonymaibox@yahoo.com>
2016-09-21 20:02:02 -0400

    Done.

--- index.js
+++ index.js
@@ -16,3 +16,70 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+var totalBatteries = batteryBatches.reduce(function(a, b){
+  return a + b
+},0);
+
+// var wordCountMap = function(){
+//   var newArray = []
+//   var lineCount = ""
+//   var arrayCount = []
+//   monologueLines.forEach(function(line){
+//     lineCount = (line.split(" "))
+//     newArray.push(lineCount.length)
+//   })
+//   // return newArray
+//   // arrayCount = 
+//   newArray.sort().reduce(function(a, b){
+//     if (a == b){
+//       arrayCount.push(a)
+//     } else {
+//       arrayCount.push(b)
+//     }
+//   })
+//   return arrayCount
+// }
+
+
+// var wordCountMap = new Object();
+var wordCountMap = new Object();
+var newArray = []
+var lineCount = ""
+  monologueLines.forEach(function(line){
+    lineCount = (line.split(" "))
+    newArray.push(lineCount.length)
+  })
+for(var i = 0; i < newArray.length; i++) {
+ if(wordCountMap[newArray[i]] != null) {
+    wordCountMap[newArray[i]] += 1;
+} else {
+    wordCountMap[newArray[i]] = 1;
+    }
+}
+
+
+
+
+// var wordCountMap = function(){
+//   monologueLines.map(function(line){
+
+//   })
+// }
+
+// var wordCountMap = function(){
+//   var newArray = []
+//   monologueLines.forEach(function(line){
+//     line.split(" ").reduce(function(a, b){
+//       return a + b
+//     }, 0)
+//     newArray.push(line)
+//   })
+//   return newArray
+// }
+
+
+
+
+
+
